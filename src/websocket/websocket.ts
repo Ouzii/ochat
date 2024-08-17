b33be7b848865d4e235960724683b69abe272994
Oskari Laaja <oskari.laaja@gmail.com>
2024-08-17 11:04:56 +0300

    Improve logging on websocket server

--- src/websocket/websocket.ts
+++ src/websocket/websocket.ts
@@ -6,25 +6,23 @@ const startWebSocketServer = (port: number) => {
     console.log(`Websocket started on port ${port}`);
   });
 
-  wsServer.on('connection', (ws, req) => {
-    console.log(wsServer.clients.size);
-    
+  wsServer.on('connection', (ws) => {
+    console.log('New connection, current client count:', wsServer.clients.size);
+
     ws.on('error', console.error);
 
     ws.on('message', (data) => {
       const msg = JSON.parse(data.toString());
       console.log(msg);
-      
+
       wsServer.clients.forEach((client, index) => {
         if (client.readyState === WebSocket.OPEN) {
           client.send(JSON.stringify(msg));
         }
       });
     });
-
-    // ws.send();
   });
-  
+
   return wsServer;
 }
 
